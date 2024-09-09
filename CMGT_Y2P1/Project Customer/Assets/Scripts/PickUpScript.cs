0d1e97b586d20d969f87c79e14a58ccd4f6b6c5d
Bram <bram.salomons@gmail.com>
2024-09-09 16:49:20 +0200

    only able to place objects on specific objects
    E to pick up, E to put down

--- CMGT_Y2P1/Project Customer/Assets/Scripts/PickUpScript.cs
+++ CMGT_Y2P1/Project Customer/Assets/Scripts/PickUpScript.cs
@@ -10,10 +10,10 @@ public class PickUpScript : MonoBehaviour
     //if you copy from below this point, you are legally required to like the video
     public float throwForce = 500f; //force at which the object is thrown at
     public float pickUpRange = 5f; //how far the player can pickup the object from
-    private float rotationSensitivity = 1f; //how fast/slow the object is rotated in relation to mouse movement
+    //private float rotationSensitivity = 1f; //how fast/slow the object is rotated in relation to mouse movement
     private GameObject heldObj; //object which we pick up
     private Rigidbody heldObjRb; //rigidbody of object we pick up
-    private bool canDrop = true; //this is needed so we don't throw/drop object when rotating the object
+    //private bool canDrop = true; //this is needed so we don't throw/drop object when rotating the object
     private int LayerNumber; //layer index
 
     //Reference to script which includes mouse movement of player (looking around)
@@ -46,25 +46,27 @@ public class PickUpScript : MonoBehaviour
             }
             else
             {
-                if (canDrop == true)
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange, ~(1 << LayerNumber)))
                 {
-                    StopClipping(); //prevents object from clipping through walls
-                    DropObject();
+                    //make sure right tag is attached
+                    if (hit.transform.gameObject.tag == "canBePlacedOn")
+                    {
+                        //pass in placement target object into the PlaceObject function
+                        PlaceObject(hit.transform.gameObject);
+                    }
                 }
+
             }
         }
         if (heldObj != null) //if player is holding object
         {
             MoveObject(); //keep object position at holdPos
-            RotateObject();
-            if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop == true) //Mous0 (leftclick) is used to throw, change this if you want another button to be used)
-            {
-                StopClipping();
-                ThrowObject();
-            }
 
+            //RotateObject();   ///////// do we keep object rotation?
         }
     }
+
     void PickUpObject(GameObject pickUpObj)
     {
         if (pickUpObj.GetComponent<Rigidbody>()) //make sure the object has a RigidBody
@@ -78,21 +80,25 @@ public class PickUpScript : MonoBehaviour
             Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
         }
     }
-    void DropObject()
+
+    void PlaceObject(GameObject placeOnObj)
     {
-        //re-enable collision with player
         Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
         heldObj.layer = 0; //object assigned back to default layer
         heldObjRb.isKinematic = false;
         heldObj.transform.parent = null; //unparent object
+        heldObj.transform.position = placeOnObj.transform.position;
         heldObj = null; //undefine game object
+
     }
+
     void MoveObject()
     {
         //keep object position the same as the holdPosition position
         heldObj.transform.position = holdPos.transform.position;
     }
-    void RotateObject()
+
+    /*void RotateObject()
     {
         if (Input.GetKey(KeyCode.R))//hold R key to rotate, change this to whatever key you want
         {
@@ -115,30 +121,7 @@ public class PickUpScript : MonoBehaviour
             //mouseLookScript.lateralSensitivity = originalvalue;
             canDrop = true;
         }
-    }
-    void ThrowObject()
-    {
-        //same as drop function, but add force to object before undefining it
-        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
-        heldObj.layer = 0;
-        heldObjRb.isKinematic = false;
-        heldObj.transform.parent = null;
-        heldObjRb.AddForce(transform.forward * throwForce);
-        heldObj = null;
-    }
-    void StopClipping() //function only called when dropping/throwing
-    {
-        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position); //distance from holdPos to the camera
-        //have to use RaycastAll as object blocks raycast in center screen
-        //RaycastAll returns array of all colliders hit within the cliprange
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
-        //if the array length is greater than 1, meaning it has hit more than just the object we are carrying
-        if (hits.Length > 1)
-        {
-            //change object position to camera position 
-            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f); //offset slightly downward to stop object dropping above player 
-            //if your player is small, change the -0.5f to a smaller number (in magnitude) ie: -0.1f
-        }
-    }
+    }*/
+
+
 }
